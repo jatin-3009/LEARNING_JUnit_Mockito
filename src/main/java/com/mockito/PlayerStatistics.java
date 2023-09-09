337659b1f3262dde18c3e23154a4d38705010aaf
jatin-3009 <jatinninawe02@gmail.com>
2023-09-09 13:02:16 +0530

    Use case of @Before annotation and different assertions

---
+++ src/main/java/com/mockito/PlayerStatistics.java
@@ -0,0 +1,70 @@
+package com.mockito;
+
+public class PlayerStatistics {
+    private Player player;
+    private int games;
+    private int goals;
+
+    public PlayerStatistics(Player player, int games, int goals) {
+        this.player = player;
+        this.games = games;
+        this.goals = goals;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public int getGames() {
+        return games;
+    }
+
+    public void setGames(int games) {
+        this.games = games;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public boolean underThirty() {
+        return this.player.getAge() < 30;
+    }
+
+    public static Player getYoungerPlayer(Player player1, Player player2) {
+        int ageOfPlayer1 = player1.getAge();
+        int ageOfPlayer2 = player2.getAge();
+
+        if (ageOfPlayer1 < ageOfPlayer2) {
+            return player1;
+        } else if (ageOfPlayer2 < ageOfPlayer1) {
+            return player2;
+        } else {
+            return null;
+        }
+    }
+
+    public double gamesPerGoal() {
+        return (double) this.games / (double) this.goals;
+    }
+
+    public double goalsPerGame() {
+        return (double) this.goals / (double) this.games;
+    }
+
+    public Double[] createCsvRecord() {
+        if (this.games == 0 || this.goals == 0) {
+            return null;
+        } else {
+            return new Double[]{goalsPerGame(), gamesPerGoal()};
+        }
+    }
+}
---
+++ src/test/java/PlayerStatisticsTest.java
@@ -0,0 +1,72 @@
+import com.mockito.Player;
+import com.mockito.PlayerStatistics;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class PlayerStatisticsTest {
+
+    private Player playerShawnAbove30;
+    private PlayerStatistics statisticsOfShawnAbove30;
+
+    @Before
+    public void setup() {
+        playerShawnAbove30 = new Player("Shawn", 35);
+        statisticsOfShawnAbove30 = new PlayerStatistics(playerShawnAbove30, 3, 3);
+    }
+
+    @Test
+    public void playerNameEqual() {
+        Player player2 = new Player("Shawn", 25);
+        assertEquals(playerShawnAbove30, player2);
+    }
+
+    @Test
+    public void playerNameNotEqual() {
+        Player player2 = new Player("Axel", 35);
+        assertNotEquals(playerShawnAbove30, player2);
+    }
+
+    @Test
+    public void youngerPlayerSame() {
+        Player player2 = new Player("Shawn", 20);
+        assertSame(player2, PlayerStatistics.getYoungerPlayer(playerShawnAbove30, player2));
+    }
+
+    @Test
+    public void underThirtyTrue() {
+        Player player = new Player("Axel", 25);
+        PlayerStatistics statistics = new PlayerStatistics(player, 10, 10);
+        assertTrue(statistics.underThirty());
+    }
+
+    @Test
+    public void underThirtyFalse() {
+        assertFalse(statisticsOfShawnAbove30.underThirty());
+    }
+
+    @Test
+    public void csvReportNull() {
+        Player player = new Player("Axel", 25);
+        PlayerStatistics statistics = new PlayerStatistics(player, 0, 0);
+        assertNull(statistics.createCsvRecord());
+    }
+
+    @Test
+    public void csvReportNotNull() {
+        Player player = new Player("Axel", 25);
+        PlayerStatistics statistics = new PlayerStatistics(player, 3, 3);
+        assertNotNull(statistics.createCsvRecord());
+    }
+
+    @Test
+    public void getCsvStatsRecord() {
+        Player player = new Player("Axel", 25);
+        PlayerStatistics statistics = new PlayerStatistics(player, 4, 8);
+        Double[] expectedArray = {2d, 0.5};
+        assertArrayEquals(expectedArray, statistics.createCsvRecord());
+    }
+}
